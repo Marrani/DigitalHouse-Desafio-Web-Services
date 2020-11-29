6eecd6397dbc75d1d74002d438c9e04d24cf300e
Marrani <gabrielmarrani@gmail.com>
2020-11-28 23:52:16 -0300

    Layout login

--- Marvel/app/src/main/java/com/gabrielmarrani/marvel/MainActivity.kt
+++ Marvel/app/src/main/java/com/gabrielmarrani/marvel/MainActivity.kt
@@ -7,5 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+        transaction.add(R.id.fragmentContainer, LoginFragment())
+        transaction.commit()
+
     }
 }
